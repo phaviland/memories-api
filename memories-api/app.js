42f323876338f2090a38e66445d84749e8744fa5
phaviland <25873346+phaviland@users.noreply.github.com>
2019-08-04 01:45:13 -0400

    Update app.js

--- memories-api/app.js
+++ memories-api/app.js
@@ -1,26 +1,38 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
-const mime = require('mime');
+const mongoose = require('mongoose'),
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    multerS3 = require('multer-s3'),
+    aws = require('aws-sdk'),
+    mime = require('mime'),
+    bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken');
 
 app.listen(3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, dbName: 'app' });
+mongoose.set('useFindAndModify', false);
+
+const saltRounds = 10;
 
 const memorySchema = {
-    userId: Number,
+    username: String,
     title: String,
     info: String,
     photos: [String],
     creationTimestamp: Date
 };
+const userSchema = {
+    username: {
+        type: String, unique: true
+    },
+    hash: String
+};
 const memoryModel = mongoose.model("memories", memorySchema);
+const userModel = mongoose.model("users", userSchema);
 
 aws.config.update({
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
@@ -42,64 +54,138 @@ const upload = multer({
     })
 });
 
-// Register
+const ProtectedRoutes = express.Router();
+app.use('/secure', ProtectedRoutes);
 
-// Login
+ProtectedRoutes.use((req, res, next) => {
+    var token = req.headers['access-token'];
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ Error: 'Invalid token.' });
+        } else {
+            req.decoded = decoded;
+            next();
+        }
+    });
+});
 
-// Logout
+app.post('/register', function (req, res, next) {
+    if (req.body.username == null || !req.body.username.trim())
+        return res.status(400).send({ Error: 'Missing username.' });
+    if (req.body.password == null || !req.body.password.trim())
+        return res.status(400).send({ Error: 'Missing password.' });
 
-// Create
-app.post('/creatememory', upload.array('photo', 4), function (req, res, next) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        let user = new userModel({
+            username: req.body.username,
+            hash: hash            
+        });
+
+        user.save(function (err) {
+            if (err && err.code == 11000)
+                return res.status(200).send({ Error: 'Username already exists.' });
+            else if (err)
+                return res.status(500).send(err);
+            else
+                return res.status(201).send(user);
+        });
+    });
+});
+
+app.post('/login', function (req, res, next) {
+    userModel.findOne({ username: req.body.username }, function (err, user) {
+        if (err)
+            return res.status(500).send(err);
+        else if (user == null)
+            return res.status(401).json({ Error: 'Invalid password/username.' });
+        else {
+            bcrypt.compare(req.body.password, user.hash, function (err, cryptres) {
+                if (err)
+                    return res.status(500).send(err);
+                else if (cryptres == false)
+                    return res.status(401).json({ Error: 'Invalid password/username.' });
+                else {
+                    const payload = {
+                        username: req.body.username
+                    };
+
+                    var token = jwt.sign(payload, process.env.SECRET, {
+                        //expiresIn: 1440 // expires in 24 hours
+                    });
+
+                    return res.status(200).json({ Token: token });
+                }
+            });
+        }
+    });    
+});
+
+app.post('/secure/memory', upload.array('photo', 4), function (req, res, next) {
     let memory = new memoryModel({
-        userId: 1,
+        username: req.decoded.username,
         title: req.body.title,
         info: req.body.info,
         photos: req.files.map(({ key }) => key),
         creationTimestamp: new Date()
     });
+
     memory.save(function (err) {
         if (err)
-            res.status(500).send(err);
+            return res.status(500).send(err);
         else
-            res.status(201).send(memory);
-    });
-})
-
-// Read
-app.get("/getmemory", function (req, res) {
-    memoryModel.find({ userId: req.query.userid }, null, { skip: parseInt(req.query.offset), sort: { creationTimestamp: -1 } }, function (err, foundMemories) {
-        if (err)
-            res.status(500).send(err);
-        else
-            res.status(200).send(foundMemories);
+            return res.status(201).send(memory);
     });
 });
 
-// Update
-app.post('/updatememory', upload.array('photo', 4), function (req, res, next) {
-    memoryModel.updateOne({ _id: req.body._id }, {
+app.get("/secure/memory", function (req, res, next) {
+    memoryModel.find({ username: req.decoded.username }, null, { skip: parseInt(req.query.offset), sort: { creationTimestamp: -1 }, limit: 5 }, function (err, memories) {
+        if (err)
+            return res.status(500).send(err);
+        else
+            return res.status(200).send(memories);
+    });
+});
+
+app.put('/secure/memory/:memoryId', upload.array('photo', 4), function (req, res, next) {
+    memoryModel.findOneAndUpdate({ _id: req.params.memoryId, username: req.decoded.username}, {
         $set: {
-            userId: 1,
             title: req.body.title,
             info: req.body.info,
             photos: req.files.map(({ key }) => key)
         }
-    } , { upsert: true }, function (err, results) {
+    }, function (err, memory) {
         if (err)
-            res.status(500).send(err);
+            return res.status(500).send(err);
+        else if (memory == null)
+            return res.status(410).json({ Error: 'Memory does not exist.' });
         else
-            res.status(200).send(results);
-    })
-})
+            return res.status(200).send({});
+    });
+});
 
-// Delete
-app.post('/deletememory', function (req, res, next) {
-    console.log(req.body._id)
-    memoryModel.deleteOne({ _id: req.body._id }, function (err, results) {
+app.delete('/secure/memory/:memoryId', function (req, res, next) {
+    memoryModel.findOneAndDelete({ _id: req.params.memoryId, username: req.decoded.username }, function (err, memory) {
         if (err)
-            res.status(500).send(err);
-        else
-            res.status(200).send(results);
-    })
-})
+            return res.status(500).send(err);
+        else if (memory == null)
+            return res.status(410).json({ Error: 'Memory does not exist.' });
+        else if (memory.photos.length != 0) {
+            let photos = [];
+            memory.photos.forEach(function (fileName) {
+                photos.push({ Key: fileName });
+            });
+            let params = {
+                Bucket: 'memories-api', Delete: { Objects: photos }
+            };
+            s3.deleteObjects(params, function (err, data) {
+                if (err)
+                    return res.status(500).send(err);
+                else
+                    return res.status(200).json({});
+            });
+        } else {
+            return res.status(200).json({});
+        }
+    });
+});
 
